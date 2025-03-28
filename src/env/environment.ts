ed59e6efe86132370431e5041952c01dd671a501
elliot <elliot@norrevik.se>
2025-03-28 20:36:59 +0100

    bruh

--- src/env/environment.ts
+++ src/env/environment.ts
@@ -1,11 +1,13 @@
 interface Environment {
   GEMINI_API_KEY: string;
   GITHUB_TOKEN: string;
+  GEMINI_PROMPT: string;
 }
 
 const env: Environment = {
   GEMINI_API_KEY: process.env.GEMINI_API_KEY || '',
-  GITHUB_TOKEN: process.env.GITHUB_TOKEN || ''
+  GITHUB_TOKEN: process.env.GITHUB_TOKEN || '',
+  GEMINI_PROMPT: process.env.GEMINI_PROMPT || ''
 };
 
 export default env;
